a06a598181f8728c1de335b4c59aa5ea83864e9c
INFRAGISTICS\ZNikolov <znikolov@infragistics.com>
2016-10-28 14:28:31 +0300

    adding MasterDetailPage

--- xPlatCHM/xPlatCHM/Views/CasePage.xaml.cs
+++ xPlatCHM/xPlatCHM/Views/CasePage.xaml.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using xPlatCHM.DataModels;
 
 namespace xPlatCHM.Views
 {
-    public partial class CasesView : MasterDetailPage
+    public partial class CasePage : ContentPage
     {
-        public CasesView()
+        public CasePage()
         {
             InitializeComponent();
+            
         }
     }
 }

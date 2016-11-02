78a76a2b4f23655c3a5da3d3fb4da060ca7d6cfe
Borislav Kulov <bkulov@infragistics.com>
2016-11-02 10:10:22 +0200

    Return some dummy data for cases until the login is working

--- xPlatCHM/xPlatCHM/Services/DataService.cs
+++ xPlatCHM/xPlatCHM/Services/DataService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using xPlatCHM.DataModels;
 
 namespace xPlatCHM.Services
@@ -75,20 +76,22 @@ namespace xPlatCHM.Services
 
 		public IEnumerable<Case> GetCases(string queryName)
 		{
-			try
-			{
-				return this.GetCachedCases(queryName);
-			}
-			catch (ArgumentOutOfRangeException ex) when (ex.ParamName == nameof(queryName))
-			{
-				return this.LoadCases(queryName);
-			}
+			// TODO: Enable this code when the authentication is working
+			//var cachedCases = this.GetCachedCases(queryName);
+			//if (cachedCases.Any())
+			//	return cachedCases;
+
+			//return this.LoadCases(queryName);
+
+			return this.CreateTestCases();
 		}
 
 		public IEnumerable<Case> LoadCases(string queryName)
 		{
 			using (var client = new HttpClient())
 			{
+				var auth = Views.LoginView.AuthResult;
+
 				var response = client.GetAsync(CRMBaseAddress + CasesRelativePath).Result;
 
 				if (!response.IsSuccessStatusCode)
@@ -117,7 +120,7 @@ namespace xPlatCHM.Services
 				throw new ArgumentNullException(nameof(queryName));
 
 			if (!this.mapQueryToListOfCases.ContainsKey(queryName))
-				throw new ArgumentOutOfRangeException(nameof(queryName));
+				return Enumerable.Empty<Case>();
 
 			return this.mapQueryToListOfCases[queryName];
 		}
@@ -196,6 +199,93 @@ namespace xPlatCHM.Services
 
 				yield return activity;
 			}
+		}
+
+		private IEnumerable<Case> CreateTestCases()
+		{
+			yield return new Case
+			{
+				AssignedTo = "System User, Infragistics",
+				CaseNumber = "CAS-177774-S8K2B5",
+				Customer = new Customer
+				{
+					Name = "Xkbjye, Cgyqg",
+					Company = "Project DocControl"
+				},
+				Component = "WebDataGrid",
+				CreatedOn = DateTime.Parse("2016-10-05T13:56:23"),
+				Description = "<p>I have a webdatagrid that has a list of companies. &nbsp;When I select a company, I need for the contacts list to update. &nbsp;The load event is not firing on my website. &nbsp;It's firing on my local web site, but when I upload the pages, it gives me a server does not respond error.</p>",
+				DueBy = DateTime.Parse("2016-10-10T13:00:00"),
+				ForumThreadID = null,
+				ID = "8ea4ce04-258b-e611-924f-000c295bab63",
+				IsCaseVisibleToCustomer = true,
+				Priority = Priority.Normal,
+				Product = "Infragistics ASP.NET 2016 Vol. 1",
+				ProductBuild = null,
+				SalesOwner = "Kowitz, Jason",
+				ServiceLevel = ServiceLevel.Standard,
+				Status = Status.InQueue,
+				StepsToReproduce = "",
+				Team = "DS - Web",
+				Technology = "ASPNET",
+				Title = "I'm trying to cascading dropdowns and it's not firing on the website"
+			};
+
+			yield return new Case
+			{
+				AssignedTo = "System User, Infragistics",
+				CaseNumber = "CAS-177776-Y1W7B6",
+				Customer = new Customer
+				{
+					Name = "oyasagmnr, uqykd",
+					Company = "Casamba Inc."
+				},
+				Component = "Data Grid",
+				CreatedOn = DateTime.Parse("2016-10-05T15:28:35"),
+				Description = "<p>Hi,</p>< p ></ p >< p > Is there a way in Infragistics UWP Data grid to select cells instead of rows ?</ p >< p ></ p >< p > Thanks.</ p >",
+				DueBy = DateTime.Parse("2016-10-06T15:00:00"),
+				ForumThreadID = null,
+				ID = "44d3c6e5-318b-e611-924f-000c295bab63",
+				IsCaseVisibleToCustomer = false,
+				Priority = Priority.Normal,
+				Product = "Infragistics UWP August",
+				ProductBuild = null,
+				SalesOwner = "Haddad, Joseph",
+				ServiceLevel = ServiceLevel.Trial,
+				Status = Status.InQueue,
+				StepsToReproduce = "",
+				Team = "DS - Xsharp",
+				Technology = "Other",
+				Title = "Data Grid Selection mode cell"
+			};
+
+			yield return new Case
+			{
+				AssignedTo = "System User, Infragistics",
+				CaseNumber = "CAS-177777-Z0D0B8",
+				Customer = new Customer
+				{
+					Name = "Uoaruaw, Npri",
+					Company = "NEW YORK LIFE"
+				},
+				Component = "WebDataGrid",
+				CreatedOn = DateTime.Parse("2016-10-05T15:31:31"),
+				Description = "<p>Team,</p><p>We have editable webdatagrid with batch update mode enabled.</p><p>Initially, user inserted 1st record and clicked on external save button, Record saved successfully to the database.&nbsp;</p><p>Now user is trying to insert 2nd record and clicked on external save button. As per business logic in that scenario, grid should contain only 1 record. so server side business logic thrown exception.&nbsp;</p><p>Then on client user removed the 2nd record and it is removing successfully on client side. But when user clicks on external save button, Still on server side grid is showing 2 records. We are not understanding why it is still showing 2 records.&nbsp;</p><p>Could you please let us know what to do be done for this. &nbsp;&nbsp;</p>",
+				DueBy = DateTime.Parse("2016-10-06T15:00:00"),
+				ForumThreadID = null,
+				ID = "44cb384d-328b-e611-924f-000c295bab63",
+				IsCaseVisibleToCustomer = true,
+				Priority = Priority.Normal,
+				Product = "Infragistics ASP.NET 2016 Vol. 1",
+				ProductBuild = null,
+				SalesOwner = "Marotta, Sam",
+				ServiceLevel = ServiceLevel.Priority,
+				Status = Status.InQueue,
+				StepsToReproduce = "",
+				Team = "DS - Web",
+				Technology = "ASPNET",
+				Title = "Webdatgrid data is not updating properly on postback"
+			};
 		}
 	}
 }
